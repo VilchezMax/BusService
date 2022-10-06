935ad60c8b0359d721bf25b6e1a3f9e3ddc70f24
VilchezMax <maximiliano.vilchez@outlook.com>
2022-10-06 00:21:24 -0300

    Added draft of models. SQL: changed boolean to tinyint

---
+++ src/main/java/busservice/models/City.java
@@ -0,0 +1,25 @@
+package busservice.models;
+
+public class City {
+    private int id;
+    private String name;
+
+    public City() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
