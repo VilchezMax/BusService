67f21c3e5303130ba3afac1cc9b6588639e6059b
VilchezMax <maximiliano.vilchez@outlook.com>
2022-10-08 14:17:26 -0300

    .

--- src/main/java/GUI/GUI.java
+++ src/main/java/GUI/GUI.java
@@ -6,6 +6,7 @@ import java.awt.*;
 public class GUI {
 
     public GUI() {
+        JFrame frame = new JFrame("My First GUI");
         JPanel panel = new JPanel();
         JButton buttonRetiro = new JButton("Retiro");
         JButton buttonCallao = new JButton("Callao");
@@ -13,7 +14,6 @@ public class GUI {
         JButton buttonWaterloo = new JButton("Waterloo");
         JLabel labelCity1 = new JLabel("Choose bus stop from city 1");
         JLabel labelCity2 = new JLabel("Choose bus stop from city 2");
-        JFrame frame = new JFrame("My First GUI");
 
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(4/*(stations.length/2)+1*/, 2, 10, 10));
@@ -25,13 +25,11 @@ public class GUI {
 
         panel.add(buttonCallao);
         panel.add(buttonOxfordCircus);
+
         /*for (int i = 0; i < stations.length; i++) {
             panel.add(new JButton(stations[i]));
 
         }*/
-
-
-
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
