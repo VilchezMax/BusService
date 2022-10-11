5031bb72d44b53f810eefc802bb0556cbe8978c6
iscreem <nahuel_1.5@outlook.com>
2022-10-11 20:56:53 -0300

    fixes of DijkstraTest

--- src/main/java/busservice/VertexTableRow.java
+++ src/main/java/busservice/VertexTableRow.java
@@ -2,46 +2,34 @@ package busservice;
 
 import java.text.DecimalFormat;
 
-public class VertexTable{
-
+public class VertexTableRow {
     private String vertex;
     private double shortestFromStart;
     private String prevVertex;
-
-    public VertexTable() {
+    public VertexTableRow() {
         this.shortestFromStart = Double.POSITIVE_INFINITY;
     }
-
     public String getVertex() {
         return vertex;
     }
-
     @Override
     public String toString() {
         DecimalFormat df = new DecimalFormat("#.##");
-        return "VertexTable{" + "vertex='" + vertex + '\'' + ", shortestFromStart=" + df.format(shortestFromStart) + ", prevVertex='" + prevVertex + '\'' + '}';
+        return "VertexTableRow{" + "vertex='" + vertex + '\'' + ", shortestFromStart=" + df.format(shortestFromStart) + ", prevVertex='" + prevVertex + '\'' + '}';
     }
-
     public void setVertex(String vertex) {
         this.vertex = vertex;
     }
-
     public double getShortestFromStart() {
         return shortestFromStart;
     }
-
     public void setShortestFromStart(double shortestFromStart) {
         this.shortestFromStart = shortestFromStart;
     }
-
     public String getPrevVertex() {
         return prevVertex;
     }
-
     public void setPrevVertex(String prevVertex) {
         this.prevVertex = prevVertex;
     }
-
-
-
 }
