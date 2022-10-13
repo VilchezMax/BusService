467bfb3c033949f0ea4205fc468e32e656959a7e
VilchezMax <maximiliano.vilchez@outlook.com>
2022-10-12 21:52:41 -0300

    CityService done

--- src/main/java/busservice/services/mybatis/CityService.java
+++ src/main/java/busservice/services/mybatis/CityService.java
@@ -1,33 +1,65 @@
 package busservice.services.mybatis;
 
+import busservice.dao.mybatis.CityDAO;
 import busservice.models.City;
 import busservice.services.IService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public class CityService implements IService<City> {
+
+    final Logger LOGGER = LogManager.getLogger(CityService.class);
+
+    CityDAO cityDAO;
+
     @Override
     public City getById(Integer id) {
-        return null;
+        City city = null;
+        try {
+            city = cityDAO.getById(id);
+        } catch (Exception e) {
+            LOGGER.warn(e.getMessage());
+        }
+        return city;
     }
 
     @Override
     public List<City> getAll() {
-        return null;
+        List<City> cities = null;
+        try {
+            cities = cityDAO.getAll();
+        } catch (Exception e) {
+            LOGGER.warn(e.getMessage());
+        }
+        return cities;
     }
 
     @Override
     public void insert(City object) {
-
+        try {
+            cityDAO.insert(object);
+        } catch (Exception e) {
+            LOGGER.warn(e.getMessage());
+        }
     }
 
     @Override
     public void update(City object) {
-
+        try {
+            cityDAO.update(object);
+        } catch (Exception e) {
+            LOGGER.warn(e.getMessage());
+        }
     }
 
     @Override
     public void deleteById(Integer id) {
-
+        try {
+            cityDAO.deleteById(id);
+        } catch (Exception e) {
+            LOGGER.warn(e.getMessage());
+        }
     }
 }
